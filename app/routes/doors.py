eaf882efbdc471dfc5fdbb9a45288be2e5e851b7
RebelRaider <leonid.chesnikov@gmail.com>
2024-04-09 16:07:59 +0300

    commit clean trash

--- app/routes/doors.py
+++ app/routes/doors.py
@@ -6,19 +6,19 @@ from utilities import controller
 
 router = APIRouter()
 
-@router.get('/door/{id}/open')
-def open_all_dors(id: int, session = Depends(controller.connection)): 
+@router.get('/cell/{id}/open')
+def open_cell(id: int, session = Depends(controller.connection)): 
     unlock(session, id)
     return True
 
 
-@router.get('/door/{id}/status')
-def open_all_dors(id: int, session = Depends(controller.connection)):
+@router.get('/cell/{id}/status')
+def is_open(id: int, session = Depends(controller.connection)):
     res = send_single_door_status(session, id)
     return res[-1] == 0
 
 
-@router.get('/door_all/open')
-def open_all_dors(id:int = 2, session = Depends(controller.connection)):
+@router.get('/all_cells/open')
+def open_all_cells(id:int = 2, session = Depends(controller.connection)):
     open_all_locks(session, id)
     return True
